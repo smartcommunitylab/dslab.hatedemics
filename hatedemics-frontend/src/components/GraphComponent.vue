1e014ce539a091c496e0aed452d0433847702760
Matteo Chini <matteo.chini@gmail.com>
2025-02-04 14:14:44 +0100

    fixed selection node for graph

--- hatedemics-frontend/src/components/GraphComponent.vue
+++ hatedemics-frontend/src/components/GraphComponent.vue
@@ -1,42 +1,37 @@
 <script setup lang="ts">
 import { onMounted, ref, useTemplateRef, type Ref } from 'vue';
-
-// import { Graph, type GraphConfigInterface } from "@cosmograph/cosmos";
 import { Cosmograph } from '@cosmograph/cosmograph'
 import { nodes, links, type Node, type Link } from "@/services/data-gen";
 import { useChannelsStore } from '@/store/ChannelStore';
 import { watch } from 'vue'
 import { storeToRefs } from 'pinia';
 
-// import { time } from 'console';
 const channelsStore = useChannelsStore();
 const { selectedChannel } = storeToRefs(channelsStore)
 let graph: Cosmograph<Node, Link>;
-// let graph:Cosmograph;
-// let config: GraphConfigInterface<Node, Link>
+
 let config = {}
 watch(selectedChannel, (newValue,oldValue) => {
     console.log('selectedNode',newValue)
     if (newValue){
       //select node by id (from 0 to x)
       //I have long id from db and in data
-      console.log("nodes",nodes)
-      console.log("newValue.id",newValue.id)
 
-      const node = nodes.filter(n => n.name == newValue.id)[0];
+
+      const node = nodes.filter(n => {
+        return n.name == newValue.id
+      })[0];
       if (node){
       graph.selectNode(node)
       // graph.selectNodeByIndex(i);
       graph.zoomToNode(node);}
     }
   })
-// const targetElement = useTemplateRef('targetElement')
 const graphElement = useTemplateRef('graphElement')
-// const selectedNode = ref('');
 const msg = ref('Hello from GraphComponent');
 // Set the data
 onMounted(() => {
-  
+
   const config={
   backgroundColor: "#151515",
   nodeSize: 1,
@@ -49,7 +44,7 @@ onMounted(() => {
   curvedLinks: true,
   renderHoveredNodeRing: true,
   hoveredNodeRingColor:'red',
-  focusedNodeRingColor: 'yellow', 
+  focusedNodeRingColor: 'yellow',
 
   disableSimulation:true,
   simulation: {
@@ -65,20 +60,16 @@ onMounted(() => {
         // graph.zoomToNodeById(node.id);
         graph.selectNode(node);
          graph.zoomToNode(node);
-        // channelsStore.selectChannel(channelsStore.channels[Math.floor(Math.random() * channelsStore.channels.length)])
         channelsStore.selectChannel(channelsStore.channels.filter(c => {
-          // console.log(c);
-          return c.id === node.name})[0])
+          return c.id === node.name
+        })[0])
 
       } else {
         graph.unselectNodes();
         channelsStore.unselectChannel();
       }
-      console.log("Clicked node: ", node);
-      // selectedNode.value = node?.id||'';
-      //TODO use real selection and not random
     }
-  
+
 };
 
 graph = new Cosmograph(graphElement.value!, config);
@@ -100,7 +91,6 @@ setTimeout(() => {
   <v-container>
     <h1>{{ msg }}</h1>
     <div ref="graphElement"></div>
-    <!-- <div >{{ selectedNode }}</div> -->
   </v-container>
 </template>
 <style lang="css" scoped>
