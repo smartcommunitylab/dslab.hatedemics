314f89714db2ac711df57ae77da91195c8065e99
kazhamiakin <kazhamiakin@gmail.com>
2025-02-25 17:21:48 +0100

    missing fields

--- hatedemics-backend/src/main/java/it/smartcommunitylab/hatedemics/api/domain/TelegramMessage.java
+++ hatedemics-backend/src/main/java/it/smartcommunitylab/hatedemics/api/domain/TelegramMessage.java
@@ -2,6 +2,9 @@ package it.smartcommunitylab.hatedemics.api.domain;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -13,16 +16,89 @@ public class TelegramMessage {
     @Id
     private String id;
 
-    private String channel_id;
-    private Date date, fwd_from_date;
+    @JsonProperty("channel_id")
+    @Column(name = "channel_id")
+    private String channelId;
+
+    private Date date;
+
+    @JsonProperty("fwd_from_date")
+    @Column(name = "fwd_from_date")
+    private Date fwdFromDate;
     
-    private String from_id, fwd_from_id, fwd_from_msg_id, replies_to_msg_id, via_bot_id;
-    private String message, preprocessed_message, reactions, replies_to_msg_text, text_mentions, text_urls;
+    @JsonProperty("from_id")
+    @Column(name = "from_id")
+    private String fromId;
 
-    private Integer nr_forwards, nr_reactions, nr_replies, nr_views;
+    @JsonProperty("fwd_from_id")
+    @Column(name = "fwd_from_id")
+    private String fwdFromId;
 
+    @JsonProperty("fwd_from_msg_id")
+    @Column(name = "fwd_from_msg_id")
+    private String fwdFromMsgId;
 
-    private String media_type;  
+    @JsonProperty("replies_to_msg_id")
+    @Column(name = "replies_to_msg_id")
+    private String repliesToMsgId;
+
+    @JsonProperty("via_bot_id")
+    @Column(name = "via_bot_id")
+    private String viaBotId;
+
+    private String message;
+
+    @JsonProperty("preprocessed_message")
+    @Column(name = "preprocessed_message")
+    private String preprocessedMessage;
+
+    private String reactions;
+
+    @JsonProperty("replies_to_msg_text")
+    @Column(name = "replies_to_msg_text")
+    private String repliesToMsgText;
+
+    @JsonProperty("text_mentions")
+    @Column(name = "text_mentions")
+    private String textMentions;
+
+    @JsonProperty("text_urls")
+    @Column(name = "text_urls")
+    private String textUrls;
+
+    @JsonProperty("nr_forwards")
+    @Column(name = "nr_forwards")
+    private Integer nrForwards;
+
+    @JsonProperty("nr_reactions")
+    @Column(name = "nr_reactions")
+    private Integer nrReactions;
+
+    @JsonProperty("nr_replies")
+    @Column(name = "nr_replies")
+    private Integer nrReplies;
+
+    @JsonProperty("nr_views")
+    @Column(name = "nr_views")
+    private Integer nrViews;
+
+    @JsonProperty("topic_label")
+    @Column(name = "topic_label")
+    private String topicLabel;
+
+    private Double topic;
+
+    @JsonProperty("hate_label")
+    @Column(name = "hate_label")
+    private Double hateLabel;
+
+    @JsonProperty("checkworthy_label")
+    @Column(name = "checkworthy_label")
+    private Double checkworthyLabel;
+
+    @JsonProperty("media_type")
+    @Column(name = "media_type")
+    private String mediaType;  
     
     private String language;
 
@@ -34,6 +110,14 @@ public class TelegramMessage {
         this.id = id;
     }
 
+    public String getChannelId() {
+        return channelId;
+    }
+
+    public void setChannelId(String channelId) {
+        this.channelId = channelId;
+    }
+
     public Date getDate() {
         return date;
     }
@@ -42,52 +126,52 @@ public class TelegramMessage {
         this.date = date;
     }
 
-    public Date getFwd_from_date() {
-        return fwd_from_date;
+    public Date getFwdFromDate() {
+        return fwdFromDate;
     }
 
-    public void setFwd_from_date(Date fwd_from_date) {
-        this.fwd_from_date = fwd_from_date;
+    public void setFwdFromDate(Date fwdFromDate) {
+        this.fwdFromDate = fwdFromDate;
     }
 
-    public String getFrom_id() {
-        return from_id;
+    public String getFromId() {
+        return fromId;
     }
 
-    public void setFrom_id(String from_id) {
-        this.from_id = from_id;
+    public void setFromId(String fromId) {
+        this.fromId = fromId;
     }
 
-    public String getFwd_from_id() {
-        return fwd_from_id;
+    public String getFwdFromId() {
+        return fwdFromId;
     }
 
-    public void setFwd_from_id(String fwd_from_id) {
-        this.fwd_from_id = fwd_from_id;
+    public void setFwdFromId(String fwdFromId) {
+        this.fwdFromId = fwdFromId;
     }
 
-    public String getFwd_from_msg_id() {
-        return fwd_from_msg_id;
+    public String getFwdFromMsgId() {
+        return fwdFromMsgId;
     }
 
-    public void setFwd_from_msg_id(String fwd_from_msg_id) {
-        this.fwd_from_msg_id = fwd_from_msg_id;
+    public void setFwdFromMsgId(String fwdFromMsgId) {
+        this.fwdFromMsgId = fwdFromMsgId;
     }
 
-    public String getReplies_to_msg_id() {
-        return replies_to_msg_id;
+    public String getRepliesToMsgId() {
+        return repliesToMsgId;
     }
 
-    public void setReplies_to_msg_id(String replies_to_msg_id) {
-        this.replies_to_msg_id = replies_to_msg_id;
+    public void setRepliesToMsgId(String repliesToMsgId) {
+        this.repliesToMsgId = repliesToMsgId;
     }
 
-    public String getVia_bot_id() {
-        return via_bot_id;
+    public String getViaBotId() {
+        return viaBotId;
     }
 
-    public void setVia_bot_id(String via_bot_id) {
-        this.via_bot_id = via_bot_id;
+    public void setViaBotId(String viaBotId) {
+        this.viaBotId = viaBotId;
     }
 
     public String getMessage() {
@@ -98,12 +182,12 @@ public class TelegramMessage {
         this.message = message;
     }
 
-    public String getPreprocessed_message() {
-        return preprocessed_message;
+    public String getPreprocessedMessage() {
+        return preprocessedMessage;
     }
 
-    public void setPreprocessed_message(String preprocessed_message) {
-        this.preprocessed_message = preprocessed_message;
+    public void setPreprocessedMessage(String preprocessedMessage) {
+        this.preprocessedMessage = preprocessedMessage;
     }
 
     public String getReactions() {
@@ -114,68 +198,100 @@ public class TelegramMessage {
         this.reactions = reactions;
     }
 
-    public String getReplies_to_msg_text() {
-        return replies_to_msg_text;
+    public String getRepliesToMsgText() {
+        return repliesToMsgText;
     }
 
-    public void setReplies_to_msg_text(String replies_to_msg_text) {
-        this.replies_to_msg_text = replies_to_msg_text;
+    public void setRepliesToMsgText(String repliesToMsgText) {
+        this.repliesToMsgText = repliesToMsgText;
     }
 
-    public String getText_mentions() {
-        return text_mentions;
+    public String getTextMentions() {
+        return textMentions;
     }
 
-    public void setText_mentions(String text_mentions) {
-        this.text_mentions = text_mentions;
+    public void setTextMentions(String textMentions) {
+        this.textMentions = textMentions;
     }
 
-    public String getText_urls() {
-        return text_urls;
+    public String getTextUrls() {
+        return textUrls;
     }
 
-    public void setText_urls(String text_urls) {
-        this.text_urls = text_urls;
+    public void setTextUrls(String textUrls) {
+        this.textUrls = textUrls;
     }
 
-    public Integer getNr_forwards() {
-        return nr_forwards;
+    public Integer getNrForwards() {
+        return nrForwards;
     }
 
-    public void setNr_forwards(Integer nr_forwards) {
-        this.nr_forwards = nr_forwards;
+    public void setNrForwards(Integer nrForwards) {
+        this.nrForwards = nrForwards;
     }
 
-    public Integer getNr_reactions() {
-        return nr_reactions;
+    public Integer getNrReactions() {
+        return nrReactions;
     }
 
-    public void setNr_reactions(Integer nr_reactions) {
-        this.nr_reactions = nr_reactions;
+    public void setNrReactions(Integer nrReactions) {
+        this.nrReactions = nrReactions;
     }
 
-    public Integer getNr_replies() {
-        return nr_replies;
+    public Integer getNrReplies() {
+        return nrReplies;
     }
 
-    public void setNr_replies(Integer nr_replies) {
-        this.nr_replies = nr_replies;
+    public void setNrReplies(Integer nrReplies) {
+        this.nrReplies = nrReplies;
     }
 
-    public Integer getNr_views() {
-        return nr_views;
+    public Integer getNrViews() {
+        return nrViews;
     }
 
-    public void setNr_views(Integer nr_views) {
-        this.nr_views = nr_views;
+    public void setNrViews(Integer nrViews) {
+        this.nrViews = nrViews;
     }
 
-    public String getMedia_type() {
-        return media_type;
+    public String getTopicLabel() {
+        return topicLabel;
     }
 
-    public void setMedia_type(String media_type) {
-        this.media_type = media_type;
+    public void setTopicLabel(String topicLabel) {
+        this.topicLabel = topicLabel;
+    }
+
+    public Double getTopic() {
+        return topic;
+    }
+
+    public void setTopic(Double topic) {
+        this.topic = topic;
+    }
+
+    public Double getHateLabel() {
+        return hateLabel;
+    }
+
+    public void setHateLabel(Double hateLabel) {
+        this.hateLabel = hateLabel;
+    }
+
+    public Double getCheckworthyLabel() {
+        return checkworthyLabel;
+    }
+
+    public void setCheckworthyLabel(Double checkworthyLabel) {
+        this.checkworthyLabel = checkworthyLabel;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
     }
 
     public String getLanguage() {
@@ -186,12 +302,5 @@ public class TelegramMessage {
         this.language = language;
     }
 
-    public String getChannel_id() {
-        return channel_id;
-    }
-
-    public void setChannel_id(String channel_id) {
-        this.channel_id = channel_id;
-    }
-
+    
 }
