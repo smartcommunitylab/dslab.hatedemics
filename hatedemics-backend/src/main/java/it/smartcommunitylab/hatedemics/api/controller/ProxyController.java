9c7b0a3947bfff91632d2f0723d045d76583574f
kazhamiakin <kazhamiakin@gmail.com>
2025-03-05 09:54:46 +0100

    Fixes for multipart and login

--- hatedemics-backend/src/main/java/it/smartcommunitylab/hatedemics/api/controller/ProxyController.java
+++ hatedemics-backend/src/main/java/it/smartcommunitylab/hatedemics/api/controller/ProxyController.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -15,6 +16,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 @RestController
 @RequestMapping("/api/proxy")
@@ -26,8 +29,13 @@ public class ProxyController {
     @Value("${dialog.url}")
     private String dialogUrl;
 
-    @Value("${token}")
-    private String token;
+    @Value("${dialogservice.username}")
+    private String username;
+    @Value("${dialogservice.password}")
+    private String password;
+
+    private static String token;
+    private static Long expiration;
 
     @RequestMapping("/gen/**")
     public ResponseEntity<?> generationProxy(HttpServletRequest request) throws IOException {
@@ -37,7 +45,7 @@ public class ProxyController {
     @RequestMapping("/dialog/**")
     public ResponseEntity<?> dialogProxy(HttpServletRequest request) throws IOException {
         Map<String, String> customHeaders = Map.of("Authorization", createToken());
-        return proxy(request, "/api/proxy/dialog", dialogUrl, null);
+        return proxy(request, "/api/proxy/dialog", dialogUrl, customHeaders);
     }
 
     public ResponseEntity<?> proxy(HttpServletRequest request, String prefix, String endpoint, Map<String, String> customHeaders) throws IOException {
@@ -59,13 +67,39 @@ public class ProxyController {
 
         HttpMethod method = HttpMethod.valueOf(request.getMethod());
         URI uri = URI.create(targetUrl);
-        String body = request.getReader().lines().reduce("", (accumulator, actual) -> accumulator + actual);
-        org.springframework.http.HttpEntity<String> entity = new org.springframework.http.HttpEntity<>(body, headers);
 
-        return restTemplate.exchange(uri, method, entity, String.class);
+        if (request instanceof MultipartHttpServletRequest) {
+            MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+            Map<String, Object> body = new HashMap<>();
+            for (Map.Entry<String, MultipartFile> entry : multipartRequest.getFileMap().entrySet()) {
+                body.put(entry.getKey(), entry.getValue().getResource());
+            }
+            org.springframework.http.HttpEntity<Map<String, Object>> entity = new org.springframework.http.HttpEntity<>(body, headers);
+            return restTemplate.exchange(uri, method, entity, String.class);
+        } else {
+            String body = request.getReader().lines().reduce("", (accumulator, actual) -> accumulator + actual);
+            org.springframework.http.HttpEntity<String> entity = new org.springframework.http.HttpEntity<>(body, headers);
+            return restTemplate.exchange(uri, method, entity, String.class);
+        }
     }
 
     private String createToken() {
+        if (token == null || System.currentTimeMillis() > expiration) {
+            login(username, password);
+        }
         return "Bearer " + token;
+    }
+
+    private void login(String username, String password) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = dialogUrl + "/token";
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("username", username);
+        requestBody.put("password", password);
+        @SuppressWarnings({ "unchecked" })
+        Map<String,?> response = restTemplate.postForEntity(url, requestBody, Map.class).getBody();
+        token = response.get("access_token").toString();
+
+        expiration = System.currentTimeMillis() + Long.parseLong(response.get("exp").toString()) * 1000;
     }
 }
