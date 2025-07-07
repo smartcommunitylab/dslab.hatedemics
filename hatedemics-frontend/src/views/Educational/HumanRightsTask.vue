0db287ce96f7983290dc7140c06d4fcc05514af9
Matteo Chini <matteo.chini@gmail.com>
2025-07-07 16:15:30 +0200

    new art look

--- hatedemics-frontend/src/views/Educational/HumanRightsTask.vue
+++ hatedemics-frontend/src/views/Educational/HumanRightsTask.vue
@@ -1,27 +1,39 @@
 <template>
   <v-container>
-    <h1>{{ t('educational.tasks.humanRights.title') }}</h1>
-    <p>{{ t('educational.tasks.humanRights.task') }}</p>
+    <h1 class="text-h4 font-weight-bold mb-4">{{ t('educational.tasks.humanRights.title') }}</h1>
+    <p class="mb-8">{{ t('educational.tasks.humanRights.task') }}</p>
 
-    <v-row>
+    <v-row dense>
       <v-col
         v-for="(article, index) in articles"
         :key="index"
         cols="12"
         sm="6"
+        md="4"
+        lg="3"
       >
-        <v-card @click="openArticle(article)">
-          <v-card-title>{{ article.title }}</v-card-title>
+        <v-card
+          @click="openArticle(article)"
+          class="d-flex align-center justify-center text-center rounded-xl pa-4"
+          height="200"
+          elevation="5"
+          color="indigo-lighten-5"
+          hover
+        >
+          <v-card-title class="text-h6 font-weight-medium text-indigo-darken-3">
+            {{ article.title }}
+          </v-card-title>
         </v-card>
       </v-col>
     </v-row>
 
     <v-dialog v-model="dialog" max-width="600">
       <v-card>
-        <v-card-title>{{ selectedArticle?.title }}</v-card-title>
+        <v-card-title class="text-h6 font-weight-bold">{{ selectedArticle?.title }}</v-card-title>
         <v-card-text>{{ selectedArticle?.text }}</v-card-text>
         <v-card-actions>
-          <v-btn @click="dialog = false">
+          <v-spacer />
+          <v-btn color="primary" @click="dialog = false">
             {{ t('close') }}
           </v-btn>
         </v-card-actions>
@@ -29,6 +41,8 @@
     </v-dialog>
   </v-container>
 </template>
+
+
 
 <script setup lang="ts">
 import { ref } from 'vue';
