60f1417b011790e46a59d5f62c6f1bf2cba76375
Matteo Chini <matteo.chini@gmail.com>
2025-08-06 15:38:11 +0200

    new strings for human rights

--- hatedemics-frontend/src/i18n/i18n.ts
+++ hatedemics-frontend/src/i18n/i18n.ts
@@ -3,9 +3,10 @@ import en from "./en.json";
 import it from "./it.json";
 import pl from "./pl.json";
 import es from "./es.json";
+import ml from "./ml.json";
 
 function loadLocaleMessages() {
-  const locales = [{ en: en }, { it: it }, { pl: pl }, { es: es }];
+  const locales = [{ en: en }, { it: it }, { pl: pl }, { es: es },  { ml: ml }];
   const messages:any = {};
   locales.forEach((lang:any) => {
     const key:any = Object.keys(lang);
